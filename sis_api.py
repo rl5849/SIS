facc98c8a6a1219d51ad1f75e09882af5829a07b
rl5849 <rl5849@rit.edu>
2018-02-24 19:01:36 -0500

    Testing python api

--- sis_api.py
+++ sis_api.py
@@ -1,4 +1,4 @@
-import mysql.connector
+import MySQLdb
 import ConfigParser, os
 from flask import Flask, request
 from flask_restful import Resource, Api
@@ -10,9 +10,29 @@ config = ConfigParser.ConfigParser()
 config.read('./API/config.ini')
 
 
-cnx = mysql.connector.connect(user=config.get('database', 'username'), password=config.get('database', 'password'),
-                              host='127.0.0.1',
-                              database=config.get('database', 'dbname'))
-cnx.close()
+
+
 app = Flask(__name__)
 api = Api(app)
+
+
+
+class Students(Resource):
+    def get(self):
+        db = MySQLdb.connect(user=config.get('database', 'username'),
+                             password=config.get('database', 'password'),
+                             host='127.0.0.1',
+                             database=config.get('database', 'dbname'))
+
+        cur = db.cursor()
+        query = cur.execute("select * from students")
+        result = {'students':[dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+
+
+api.add_resource(Students, '/students')
+
+
+if __name__ == '__main__':
+     app.run(port='5002')
