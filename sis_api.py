0f262a1074091be84c416ba06e687f7c3641fe7f
bbchristians <bbc7909@rit.edu>
2018-02-25 16:58:35 -0500

    You'd best tell me how this gon be tested

--- sis_api.py
+++ sis_api.py
@@ -7,17 +7,12 @@ from json import dumps
 from flask import jsonify
 
 
-
-
 app = Flask(__name__)
 api = Api(app)
-
-
 
 class Students(Resource):
     config = ConfigParser.ConfigParser()
     config.read('./API/config.ini')
-
 
     def get(self):
         db = MySQLdb.connect(user=self.config.get('database', 'username'),
@@ -29,11 +24,23 @@ class Students(Resource):
         query = cur.execute("select * from students")
         #result = {'students':[dict(zip(tuple (query.keys()) ,i)) for i in query]}
         return jsonify(query)
-
-class 
-
+        
 api.add_resource(Students, '/students')
 
+class GetStudentInformation(Resource):
+    config = ConfigParser.ConfigParser()
+    config.read('./API/config.ini')
+    
+    def get(self):
+        return jsonify(
+                        student_id="123456789",
+                        student_name="Betty White",
+                        date_of_birth="Jan 17, 1922",
+                        profile_pic="https://i0.wp.com/radaronline.com/wp-content/uploads/2017/05/betty-white-secret-suitor-split-pp.jpg?fit=640%2C420&ssl=1",
+                        gender="F",
+                        graduation_year="2020",
+                      )
 
+api.add_resource(GetStudentInformation, '/GetStudentInformation')
 if __name__ == '__main__':
      app.run(port=5002)
