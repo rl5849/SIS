239dc23bd005217fb15d137474276f92d271a981
rl5849 <rl5849@rit.edu>
2018-02-27 17:58:54 -0500

    API can select students classes bases on ID

--- sis_api.py
+++ sis_api.py
@@ -1,7 +1,7 @@
 import MySQLdb
 import ConfigParser, os
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from sqlalchemy import create_engine
 from json import dumps
 from flask import jsonify
@@ -10,11 +10,19 @@ from flask import jsonify
 app = Flask(__name__)
 api = Api(app)
 
-class Students(Resource):
+
+
+###Use a student ID to get all their classes currently enrolled
+class student_class(Resource):
     config = ConfigParser.ConfigParser()
     config.read('./API/config.ini')
 
     def get(self):
+        # Get student id
+        parser = reqparse.RequestParser()
+        parser.add_argument('student_id', type=int)
+        student_id = parser.parse_args().get("student_id")
+
         db = MySQLdb.connect(user=self.config.get('database', 'username'),
                              passwd=self.config.get('database', 'password'),
                              host='129.21.208.253',
@@ -23,12 +31,15 @@ class Students(Resource):
         cur = db.cursor()
 
         # Select data from table using SQL query.
-        cur.execute("SELECT * FROM students")
+        cur.execute("SELECT * FROM classes "
+                    "LEFT JOIN student_to_class ON (classes.class_id = student_to_class.class_id) "
+                    "WHERE student_to_class.student_id = %s",
+                    [student_id])
         query = cur.fetchall()
-        result = {'students': query}
+        result = {'students_classes': query}
         return jsonify(result)
         
-api.add_resource(Students, '/students')
+api.add_resource(student_class, '/student_class')
 
 class GetStudentInformation(Resource):
     config = ConfigParser.ConfigParser()
