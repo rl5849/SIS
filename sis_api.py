22367ec92a04a4115b108a8791c4e6272adb315d
rl5849 <rl5849@rit.edu>
2018-02-27 19:45:38 -0500

    API can insert new students

--- sis_api.py
+++ sis_api.py
@@ -36,10 +36,60 @@ class student_class(Resource):
                     "WHERE student_to_class.student_id = %s",
                     [student_id])
         query = cur.fetchall()
-        result = {'students_classes': query}
+        result = {'students_classes': [dict(zip(["class_id", "name", "room_number", "capacity", "num_enrolled", "time", "course_id", "rofessor_id", "student_id ", "class_id"], i)) for i in query]}
+
         return jsonify(result)
         
 api.add_resource(student_class, '/student_class')
+
+
+###Add a new student
+### url path: /add_student?student_name=NAME&date_of_birth=2001-02-01&profile_pic=www.linked.com&gender=F&graduation_year=2018
+#TODO: CRITCAL: URL ENCODE THESE ITEMS BEFORE MAKING THE REQUEST
+class add_student(Resource):
+    config = ConfigParser.ConfigParser()
+    config.read('./API/config.ini')
+
+    def get(self):
+        # Get student info
+        parser = reqparse.RequestParser()
+        parser.add_argument('student_name', type=str)
+        parser.add_argument('date_of_birth', type=str)
+        parser.add_argument('profile_pic', type=str)
+        parser.add_argument('gender', type=str)
+        parser.add_argument('graduation_year', type=int)
+
+        parsed = parser.parse_args()
+        
+        student_name = parsed.get("student_name")
+        dob = parsed.get("date_of_birth")
+        profile_pic = parsed.get("profile_pic")
+        gender = parsed.get("gender")
+        grad_year = parsed.get("graduation_year")
+
+        db = MySQLdb.connect(user=self.config.get('database', 'username'),
+                             passwd=self.config.get('database', 'password'),
+                             host='129.21.208.253',
+                             db=self.config.get('database', 'dbname'))
+
+        cur = db.cursor()
+
+        # Select data from table using SQL query.
+        cur.execute("INSERT INTO students"
+                    "(student_name, date_of_birth, profile_pic, gender, graduation_year) "
+                    "VALUES (%s, %s, %s, %s, %s);",
+                    [student_name, dob, profile_pic, gender, grad_year])
+
+        try:
+            db.commit()
+        except MySQLdb.IntegrityError:
+            return jsonify("INSERT FAILED!")
+
+        return jsonify("SUCCESS")
+
+api.add_resource(add_student, '/add_student')
+
+
 
 class GetStudentInformation(Resource):
     config = ConfigParser.ConfigParser()
