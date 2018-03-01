d2a256207d2f03bf2f1158cfc28b9e88545d96a7
Ben Christians <bbc7909@rit.edu>
2018-03-01 17:12:18 -0500

    Adding some needed JS to nav bar

--- nav.php
+++ nav.php
@@ -41,3 +41,8 @@
 	</ul>
   </div>
 </div>
+
+<script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/what-input/dist/what-input.js"></script>
+    <script src="bower_components/foundation-sites/dist/js/foundation.js"></script>
+    <script src="js/app.js"></script>
