8a0cf2b7d7dff1dde0b56a3f05bc45b1f91bc884
Ben Christians <bbc7909@rit.edu>
2018-03-01 13:53:19 -0500

    Updated nav reference

--- account.php
+++ account.php
@@ -8,45 +8,9 @@
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
-	<div class="top-bar">
-	  <div class="top-bar-left">
-		<ul class="dropdown menu" data-dropdown-menu>
-		  <li class="menu-text">SIS++</li>
-		  <li><a href="course_list.php">Course List</a></li>
-		  <li>
-			<a href="#">Admin</a>
-			<ul class="menu vertical">
-			  <li><a href="#">Admin Action 1</a></li>
-			  <li><a href="#">Admin Action 2</a></li>
-			  <li><a href="#">Admin Action 3</a></li>
-			</ul>
-		  </li>
-		</ul>
-	  </div>
-	  <div class="top-bar-right">
-		<ul class="dropdown menu" data-dropdown-menu>
-      <!--<form action="course_search.php" method="get">-->
-        <ul class="menu">
-          <li><input type="search" placeholder="Class Search"></li>
-          <li><button type="submit" class="button" formaction="course_search.php">Search</button></li> <!-- Search functionality here-->
-        </ul>
-      <!--<form>-->
-			<li>
-				<a href="account.php">Welcome, Big boyyy</a> <!-- Name here -->
-			</li>
-			<li>
-				<a href="#"></a>
-				<ul class="menu vertical">
-				  <li><a href="account.php">Profile</a></li>
-				  <li><a href="account.php">Classes</a></li>
-				  <li><a href="https://www.linkedin.com/">LinkedIn</a></li>
-				  <li><a href="login.php">Logout</a></li> <!-- we need to make sure this actually logs them out-->
-				</ul>
-			</li>
-		</ul>
-	  </div>
-	</div>
-  <div class="grid-container">
+	<div w3-include-html="nav.php"></div>
+	
+	<div class="grid-container">
   
     <div class="grid-x grid-padding-x" style="padding-top:2%;">
   
