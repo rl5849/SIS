89d189e1115de8ae2ca0f3e94ed73305f33893a2
Ben Christians <bbc7909@rit.edu>
2018-03-01 13:40:46 -0500

    asd

--- account.php
+++ account.php
@@ -60,10 +60,9 @@
 				$student_info = file_get_contents("http://127.0.0.1:5002/GetUser?student_id=".$student_id);
 				$student_info = json_decode($student_info, true);
 			?>
-		  <li><?php echo "hello";
-					//($student_info->student_info[0]->student_name);?></li>
-		  <li><?php ?></li>
-		  <li><?php ?></li>
+		  <li><?php echo "hello";?></li> <!--($student_info->student_info[0]->student_name)-->
+		  <li></li>
+		  <li></li>
         </ul>
       </div>
       <div class="large-2 medium-2 small-3 cell">
