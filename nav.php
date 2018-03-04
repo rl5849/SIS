3c8a13be0bcdae817ba9fcc7d0b82b205c9201f4
bbchristians <bbc7909@rit.edu>
2018-03-04 13:17:19 -0500

    Changed title formatting

--- nav.php
+++ nav.php
@@ -6,7 +6,7 @@
 	?>
   <div class="top-bar-left">
 	<ul class="dropdown menu" data-dropdown-menu>
-	  <li class="menu-text">SIS++</li>
+	  <li class="menu-text"><a href="account.php">SIS++</a></li>
 	  <li><a href="course_list.php">Course List</a></li>
 	  <li>
           <?php
