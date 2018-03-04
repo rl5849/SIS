7e27fba57dab3e3b9778d6a38b3c361a57f5c646
John P. Murray <jpm7003@rit.edu>
2018-03-04 14:40:52 -0500

    changed classs to classes

--- course_view.php
+++ course_view.php
@@ -81,7 +81,7 @@
 
                 <li>Credits: <?php echo ($class_info["class_info"][0]["credits"]) ?> </li>
                 <li>Enrolled: <?php echo ($class_info["class_info"][0]["num_enrolled"]) ?> / <?php echo ($class_info["class_info"][0]["capacity"]) ?>  </li> <!-- needs getPrereqs -->
-                <li>Wait List: <?php echo count($wait_list) ?>/ 0 </li>
+                <li>Wait List: <?php echo count($wait_list) ?> / 0 </li>
                 <!-- <li>...</li> -->
               </ul>
             </div>
