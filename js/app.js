e9b483ca1b2b8b93774a4d6389a1ed4356b4a416
bbchristians <bbc7909@rit.edu>
2018-03-05 12:23:39 -0500

    Reimplemented an important statement

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+$(document).foundation();
+
 function showMessage(messageType, message) {
   var callout = document.getElementById(messageType + "-message");
   callout.innerHTML = "<p>" + message + "</p>" + callout.innerHTML;
