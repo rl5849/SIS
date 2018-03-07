cae449b330c863d92ab409c9cc6aa7056dbfb5cb
bbchristians <bbc7909@rit.edu>
2018-03-07 09:43:55 -0500

    Updated tests

--- test_config.php
+++ test_config.php
@@ -1,15 +1,14 @@
-&1F914;
-
 <?php
   echo "Config contents";
   $myfile = fopen("LinkedIn/config.ini", "r") or die("Unable to open file!");
   $readfile = fread($myfile,filesize("LinkedIn/config.ini"));
   
-  echo $readfile;
-  
-  foreach($myfile as $line) {
-    echo $line;
-  }
+  $arr = explode("\n", $readfile);
+  $client_id = explode("=", $arr[0])[1];
+  $client_secret = explode("=", $arr[0])[1];
+
+  echo $client_id;
+  echo $client_secret;
   
   fclose($myfile);
 ?>
