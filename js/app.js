b0ab49606c2c17ca173a3ae41fb599a3d60dbbfc
bbchristians <bbc7909@rit.edu>
2018-03-07 13:02:05 -0500

    Added filtering for the class list

--- js/app.js
+++ js/app.js
@@ -13,3 +13,43 @@ function makeNav(){
 function makeCallouts() {
   $("#callouts-placeholder").load("callouts.html");
 }
+
+// form thing
+$(function () {
+    var showClass = 'show';
+
+    $('input').on('checkval', function () {
+        var label = $(this).prev('label');
+        if(this.value !== '') {
+            label.addClass(showClass);
+        } else {
+            label.removeClass(showClass);
+        }
+    }).on('keyup', function () {
+        $(this).trigger('checkval');
+    });
+});
+
+
+function instantiateFilter() {
+    var filter = document.getElementById('filter');
+    if( filter == null ) {
+        return;
+    }
+
+    filter.onchange = function() {filter_list();};
+
+    function filter_list() {
+        var list_entries = document.getElementsByName('class_listing');
+        var filter_text = filter.value;
+        for (var x = 0; x < list_entries.length; x++) {
+            if (filter_text === '' || list_entries[x].innerHTML.indexOf(filter_text) > -1) {
+                list_entries[x].removeAttribute('hidden');
+            }
+            else {
+                list_entries[x].setAttribute('hidden', 'true');
+            }
+        }
+        console.log("Filtered");
+    }
+}
