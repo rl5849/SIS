b55e0ed94cf857b8f69a5734e96c517739670ba9
Ben Christians <bbc7909@rit.edu>
2018-03-08 07:49:03 -0500

    m

--- auth.php
+++ auth.php
@@ -9,7 +9,7 @@ $myfile = fopen("LinkedIn/config.ini", "r") or die("Unable to open file!");
 $readfile = fread($myfile,filesize("LinkedIn/config.ini"));
 $arr = explode("\n", $readfile);
 $client_id = explode("=", $arr[0])[1];
-$client_secret = explode("=", $arr[0])[1];
+$client_secret = explode("=", $arr[1])[1];
 
 fclose($myfile);
 
