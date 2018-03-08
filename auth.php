650353f6b7c30fe70e63030f33a9b2f370686617
Ben Christians <bbc7909@rit.edu>
2018-03-08 10:29:46 -0500

    Updated documentation and removed obsolete code

--- auth.php
+++ auth.php
@@ -1,10 +1,9 @@
 <?php
-// PHP for using the LinkedIn API
-echo "10<br/>";
-
+// Start the linkedin session
 session_name('linkedin');
 session_start();
 
+// Get the code from the linkedin query
 $code = $_GET["code"];
 
 // Parse config file for codes
@@ -16,6 +15,7 @@ $client_secret = explode("=", $arr[1])[1];
 
 fclose($myfile);
 
+// Prepare parameters for retrieving the auth token from linkedin
 $params = array(
     'client_id' => $client_id,
     'client_secret' => $client_secret,
@@ -24,26 +24,27 @@ $params = array(
     'redirect_uri' => 'https://vm344p.se.rit.edu/SIS/auth.php'
 );
 
+/* Unused headers -- idk how to implement with these
 $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded",
         'method'  => "POST"
     )
 );
-
 $context  = stream_context_create($options);
-$url = "https://www.linkedin.com/oauth/v2/accessToken?".http_build_query($params);
+*/
 
-//echo "url:".$url."<br/>";
+// Make a get request for the auth token
+$url = "https://www.linkedin.com/oauth/v2/accessToken?".http_build_query($params);
 $access_token_request = file_get_contents($url, true);
 $access_token = json_decode($access_token_request);
 
+// Add auth token to the session
 $_SESSION['access_token'] = $access_token->access_token; // guard this!
 $_SESSION['expires_in']   = $access_token->expires_in; // relative time (in seconds)
 $_SESSION['expires_at']   = time() + $_SESSION['expires_in']; // absolute time
 
-//echo $_SESSION['access_token'];
-
+// Prepare parameters to query for user info from linkedin
 $params = array(
     'oauth2_access_token' => $_SESSION['access_token'],
     'format' => 'json'
@@ -52,20 +53,20 @@ $params = array(
 $linkedin_user_info = file_get_contents("https://api.linkedin.com/v1/people/~:(first-name,last-name,id,picture-url)?".http_build_query($params));
 $linkedin_user_info = json_decode($linkedin_user_info);
 
-var_dump($linkedin_user_info);
 
 $fName = $linkedin_user_info->firstName;
 $lName = $linkedin_user_info->lastName;
 $id = $linkedin_user_info->id;
 $profilePic = $linkedin_user_info->pictureUrl;
 
+// TODO Add user to system
+
 echo $fName."<br/>";
 echo $lName."<br/>";
 echo $id."<br/>";
 echo "<img src='".$profilePic."'>";
 
-
-// Reroute user to account page
+// Reroute user to account page TODO get working
 $accountPage = "account.php";
 header('Location: '.$accountPage);
 ?>
