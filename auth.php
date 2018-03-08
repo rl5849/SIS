84c83d1b54e0c8322a46488bc9eae244e6ac8441
Ben Christians <bbc7909@rit.edu>
2018-03-07 20:38:49 -0500

    Adding a new step to authentication

---
+++ auth.php
@@ -0,0 +1,43 @@
+<?php
+// PHP for using the LinkedIn API
+
+// Get the Access Token from LinkedIn
+$code = $_GET["code"];
+
+// Parse config file for codes
+$myfile = fopen("LinkedIn/config.ini", "r") or die("Unable to open file!");
+$readfile = fread($myfile,filesize("LinkedIn/config.ini"));
+$arr = explode("\n", $readfile);
+$client_id = explode("=", $arr[0])[1];
+$client_secret = explode("=", $arr[0])[1];
+
+fclose($myfile);
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded",
+        'method'  => 'POST'
+    )
+);
+
+$params = array(
+    'grant_type' => 'authorization_code',
+    'code' => $code,
+    'client_id' => $client_id,
+    'client_secret' => $client_secret,
+    'redirect_uri' => 'https://vm344p.se.rit.edu/SIS/account.php'
+);
+
+$context  = stream_context_create($options);
+$url = "https://www.linkedin.com/oauth/v2/accessToken?" . http_build_query($params);
+echo "url:".$url;
+$access_token_request = file_get_contents($url, false, $context);
+$access_token = json_decode($access_token_request)["access_token"];
+
+echo "Message from server: ".$access_token["error_description"];
+echo "First 3 of token: ".substr($access_token, 0, 3);
+
+$linkedin_user_info = file_get_contents("https://api.linkedin.com/v1/people/~?format=json");
+$linkedin_user_info = json_decode($linkedin_user_info, true);
+echo "test".$linkedin_user_info;
+?>
