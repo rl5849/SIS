7f16b88829e2458fcf3b9bd62fffec6ea2f39d52
rl5849 <rl5849@rit.edu>
2018-03-25 12:06:00 -0400

    can view course list when not logged in

--- course_list.php
+++ course_list.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id'])){
-    echo "<meta http-equiv=\"refresh\" content=\"0;URL=login.php\" />";
-    exit();
+if (isset($_SESSION['user_id'])){
+    $user_id = $_SESSION['user_id'];
 }
 else{
-    $user_id = $_SESSION['user_id'];
-} ?>
+    $user_id = NIL;
+}
+ ?>
 <!doctype html>
 <html class="no-js" lang="en">
   <head>
