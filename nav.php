0293646c5881eea8a8e31cd2cf09e62700313f24
Ben Christians <bbc7909@rit.edu>
2018-04-05 17:35:00 -0400

    Fixed CSS on account view, Fixed nav permissions when not logged in, and slightly improved account view.

--- nav.php
+++ nav.php
@@ -12,11 +12,7 @@
 	  <li><a href="course_list.php">Course List</a></li>
 	  <li>
           <?php
-          $is_admin = file_get_contents("http://127.0.0.1:5002/CheckIfAdmin?id=".$_SESSION["user_id"]);
-          $is_admin = json_decode($is_admin, true);
-          $is_admin = $is_admin["is_admin"];
-
-          if ($is_admin == "true"){
+          if ($_SESSION["is_admin"] == "true"){
           ?>
             <a href="#">Admin</a>
             <ul class="menu vertical">
@@ -31,11 +27,15 @@
   <form action="course_search.php" method="get">
 	<ul class="menu">
 	  <li><input type="search" name="search_parameters" placeholder="Class Search"></li>
-	  <li><button type="submit" class="button" formaction="course_list.php">Search</button></li> <!-- Search functionality here-->
+	  <li><button type="submit" class="button" formaction="course_list.php">Search</button></li>
 	</ul>
   </form>
+
+        <?php
+        if ($_SESSION["user_id"] != null) {
+        ?>
 		<li>
-			<a href="account.php">Welcome, <?php echo ($user_info["student_info"][0]["student_name"])?></a> <!-- Name here -->
+			<a href="account.php">Welcome, <?php echo ($user_info["student_info"][0]["student_name"])?></a>
 		</li>
 		<li>
 			<a href="#"><i class="fi-list"></i></a>
@@ -46,6 +46,11 @@
 			  <li><a href="logout.php">Logout</a></li>
 			</ul>
 		</li>
+        <?php }  else  {?>
+        <li>
+            <a href="login.php">Log In</a>
+        </li>
+        <?php }?>
 	</ul>
   </div>
 </div>
