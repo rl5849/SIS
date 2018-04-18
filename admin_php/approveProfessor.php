69b0472e1a8bdfacd62a7b10bc3b128292a8df5c
Ben Christians <bbc7909@rit.edu>
2018-04-18 16:46:34 -0400

    Updated admin view to not suck

---
+++ admin_php/approveProfessor.php
@@ -0,0 +1,16 @@
+<div class="small-12 medium-6 large-4 columns">
+    <!-- Start new form -->
+    <form class="callout text-center" method="post">
+        <input type="hidden" name="action" value="add_semester">
+        <h4>Approve Professor Status</h4>
+        <div class="floated-label-wrapper">
+            <table class="hover">
+                <tbody id="profs">
+                <!--                              Javascript builds table here-->
+                </tbody>
+            </table>
+        </div>
+
+    </form>
+    <!-- End new form -->
+</div>
