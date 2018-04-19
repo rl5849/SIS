3e2179a6110c69baad032fb1195000272e821cf2
rl5849 <rl5849@rit.edu>
2018-04-19 13:38:39 -0400

    form for adding admin

--- admin_php/addSemester.php
+++ admin_php/addSemester.php
@@ -5,7 +5,7 @@
         <h4>Add a New Semester</h4>
         <div class="floated-label-wrapper">
             <label for="semester_code">Semester Code</label>
-            <input type="text" id="semester_code" name="semester_code" placeholder="Semester Code">
+            <input type="text" id="semester_code" name="semester_code" placeholder="Semester Code" required>
         </div>
         <input class="button expanded" type="submit" value="Create Semester">
     </form>
@@ -22,4 +22,39 @@
         <input class="button expanded" type="submit" value="Add Admin">
     </form>
     <!-- End new form -->
+    
+    <table>
+    <?php
+    $users = file_get_contents("http://127.0.0.1:5002/GetUsers");
+    $users = json_decode($users, true);
+    $users = $users['users'];
+
+    foreach ($users as $user){
+        if ($user['user_status'] == '2'){
+            continue;
+        }
+        switch ($user['user_status']){
+            case '0':
+                $status = "Student";
+                break;
+            case '1':
+                $status = "Professor";
+                break;
+            case '2':
+                $status = "Admin";
+                break;
+        }
+
+        echo "<tr> 
+                <td>" . $user['name'] . "</td> 
+                <td>" . $status . "</td> 
+                <td> 
+                    <form class='text-center' method='post'>
+                        <input type='hidden' name='action' value='make_admin'> 
+                        <input type='hidden' name='user_id' value='" . $user['user_id'] . "'> 
+                        <input class='button expanded rit-orange' type='submit' value='Make Admin'>
+                    </form> </td>
+              </tr>";
+    }?>
+    </table>
 </div>
