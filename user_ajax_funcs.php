1e0bdfe218b9a65d1108d254d06408bff03ba448
rl5849 <rl5849@rit.edu>
2018-04-29 15:04:03 -0400

    fixed ajax error

--- user_ajax_funcs.php
+++ user_ajax_funcs.php
@@ -150,7 +150,7 @@ function RequestProfStatus(){
 }
 
 function submitGrade(){
-	$results = file_get_contents("http://127.0.0.1:5002/setGrade?student_id=" .$_POST["student_id"] . "&class_id=".$_POST["class_id"] . "&grade=" .$_POST["grade"]; 
+	$results = file_get_contents("http://127.0.0.1:5002/setGrade?student_id=" .$_POST["student_id"] . "&class_id=".$_POST["class_id"] . "&grade=" .$_POST["grade"]);
 	$results = json_decode($results, true);
 	
 	if ($results == "SUCCESS"){
